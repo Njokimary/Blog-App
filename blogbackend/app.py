079b14ebd9d0b0529d80f70289986fab1c270744
Njokimary <marynjoki738@gmail.com>
2023-10-31 17:45:18 +0300

    feat:get all users and posts

--- blogbackend/app.py
+++ blogbackend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
@@ -12,46 +13,75 @@ migrate = Migrate(app, db)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    posts = db.relationship('Post', backref='author_posts', lazy='dynamic')
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    category = db.relationship('Category', backref='posts')
-    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    categories = db.relationship('Category', secondary='post_category', backref='posts', lazy=True)
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    posts = db.relationship('Post', backref='category', lazy='dynamic')
+    name = db.Column(db.String(50), unique=True, nullable=False)
 
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
-    author = db.relationship('User', backref='comments')
+post_category = db.Table('post_category',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
 
 
+# Create a new user
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(username=data['username'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully!'})
+
+# Get all users
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {}
+        user_data['id'] = user.id
+        user_data['username'] = user.username
+        user_data['posts'] = [post.title for post in user.posts]
+        output.append(user_data)
+    return jsonify({'users': output})
 
 # Create a new post
 @app.route('/posts', methods=['POST'])
 def create_post():
     data = request.get_json()
-    title = data['title']
-    content = data['content']
-    author_id = data['author_id']
-    category_id = data['category_id']
-    post = Post(title=title, content=content, author_id=author_id, category_id=category_id)
-    db.session.add(post)
+    new_post = Post(title=data['title'], content=data['content'], user_id=data['user_id'])
+    db.session.add(new_post)
     db.session.commit()
     return jsonify({'message': 'Post created successfully!'})
+
+##get all posts
+
+@app.route('/posts', methods=['GET'])
+def get_all_posts():
+    posts = Post.query.all()
+    output = []
+    for post in posts:
+        post_data = {
+            'id': post.id,
+            'title': post.title,
+            'content': post.content,
+            'date_posted': post.date_posted,
+            'author': post.author.username,
+            'categories': [category.name for category in post.categories]
+        }
+        output.append(post_data)
+    return jsonify({'posts': output})
 
 
 
